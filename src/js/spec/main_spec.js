a0e0ecce46b0a1943ac5542381420fd870314b23
Emibob <emily.obrien@refinery29.com>
2015-02-15 16:27:56 -0500

    very basic testing started

--- src/js/spec/main_spec.js
+++ src/js/spec/main_spec.js
@@ -1,5 +1,30 @@
-describe("testing", function(){
-	it("should run my test and pass", function(){
-		expect(true).toEqual(true);
+describe("the lingo dictionary", function(){
+	
+	beforeEach(function(){
+		var lingo = window.lingo;
 	});
+
+	describe("when the lingo library is loaded", function(){
+		it("contains the array ts", function(){
+			expect(lingo.ts.length).toBeGreaterThan(0);
+		});
+		it("contains the array nineteenEightyNine", function(){
+			expect(lingo.nineteenEightyNine.length).toBeGreaterThan(0);
+		});
+		it("contains the array red", function(){
+			expect(lingo.red.length).toBeGreaterThan(0);
+		});
+		it("contains the array speakNow", function(){
+			expect(lingo.speakNow.length).toBeGreaterThan(0);
+		});
+		it("contains the array fearless", function(){
+			expect(lingo.fearless.length).toBeGreaterThan(0);
+		});
+		it("contains the array taylorSwift", function(){
+			expect(lingo.taylorSwift.length).toBeGreaterThan(0);
+		});
+		it("contains the array soundsoftheseason", function(){
+			expect(lingo.soundsoftheseason.length).toBeGreaterThan(0);
+		});
+	});	
 });
